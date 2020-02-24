a400de6ab5bdc5290266bce4cd0ef1711d153c57
Jared Salazar Martinez <33723377+TateTipycsnow@users.noreply.github.com>
2020-02-24 16:08:30 -0600

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { ZombiesComponent } from './pages/zombies/zombies.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraphsComponent } from './pages/graphs/graphs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidemenuComponent } from './shared/sidemenu/sidemenu.component';
+import { SettingsComponent } from './shared/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZombiesComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    GraphsComponent,
+    HeaderComponent,
+    SidemenuComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/zombies/zombies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zombies',
+  templateUrl: './zombies.component.html',
+  styleUrls: ['./zombies.component.css']
+})
+export class ZombiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
